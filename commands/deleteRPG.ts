d80b1b6f3b1bb19eb83fb04c0e0eaabbac3e0e6e
LucasLadeiraL2 <lucas.ladeira@l2code.com.br>
2025-07-10 04:06:00 -0300

    FEAT: delete and update added

--- commands/deleteRPG.ts
+++ commands/deleteRPG.ts
@@ -1,7 +1,19 @@
 import db from '../data/database';
+import { RPGTable } from '../data/tableTypes';
 
 function deleteRPG(abbreviation:string, user_id:string): string {
-    return '';
+    const check = db.prepare(`SELECT 1 FROM rpg WHERE abbreviation = ?`).get(abbreviation) as RPGTable | undefined;
+
+    if (!check) {
+        return `RPG **${abbreviation}** not found.`;
+    }
+
+    if (check.user_id === user_id) {
+        db.prepare(`DELETE FROM rpg WHERE abbreviation = ?`).run(abbreviation);
+        return `RPG **${abbreviation}** has been deleted successfully.`;
+    } else {
+        return `You do not have permission to delete the RPG **${abbreviation}**.`;
+    }
 }
 
 export { deleteRPG };
--- commands/updateRPG.ts
+++ commands/updateRPG.ts
@@ -1,7 +1,12 @@
 import db from '../data/database';
+import { deleteRPG } from './deleteRPG';
+import { createRPG } from './createRPG';
 
 function updateRPG(abbreviation: string, user_id: string, topics: string): string {
-    return '';
+    deleteRPG(abbreviation, user_id);
+    createRPG(abbreviation, user_id, topics);
+    
+    return `RPG **${abbreviation}** has been updated successfully with new topics.`;
 }
 
 export { updateRPG };
