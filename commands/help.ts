673f2cd54a412908849cfbe63f3c5df82d999cda
LucasLadeiraL2 <lucas.ladeira@l2code.com.br>
2025-07-10 03:48:28 -0300

    FIX: Adjust topic text

--- commands/help.ts
+++ commands/help.ts
@@ -1,9 +1,10 @@
 function help() {
     return `**Criador de Tópico Help**
 - **$create $<rpgName> $[topics]**: Create a new RPG with the specified name and optional topics. If topics are not provided, you will be prompted to confirm the creation of topics in the current channel.
-Example: \`$create $RPGName $ 
+Example: 
+\`\`\`$create $RPGName $ 
 Topic1 
-Topic2\`
+Topic2\`\`\`
 - **$list**: List all RPGs available in the database.`;
 }
 
