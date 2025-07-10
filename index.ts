e66fac5775c71eee3d4a14cce4006e0e4cafae23
LucasLadeiraL2 <lucas.ladeira@l2code.com.br>
2025-07-10 01:48:06 -0300

    ajuste indices

--- index.ts
+++ index.ts
@@ -24,13 +24,13 @@ client.once("ready", () => {
 client.on('messageCreate', (message) => {
   const MessageContent = message.content.toLowerCase().trim();
   const MessageCommands = MessageContent.split('$');
-  const firstCommand = MessageCommands[0].trim();
+  const firstCommand = MessageCommands[1].trim();
   console.log(`Received command: ${firstCommand}`);
-  
+
   if (firstCommand === 'create') {
-    const rpgName = MessageCommands[1].trim();
+    const rpgName = MessageCommands[2].trim();
     if (rpgName) {
-      const topics = MessageCommands[2].trim();
+      const topics = MessageCommands[3].trim();
       if (topics) {
         message.reply(createRPG(rpgName, message.author.id, topics));
       }
